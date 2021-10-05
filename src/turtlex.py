58d2579a55750a2da58049b94ddcbccda65ce7bf
edbotstudio <studio@ed.bot>
2021-10-05 20:17:06 +0100

    Documented source file and renamed maximise() to restore().

--- src/turtlex.py
+++ src/turtlex.py
@@ -4,41 +4,75 @@
 # Copyright (c) Robots in Schools Ltd. All rights reserved.
 #
 
+#
+# Send the window to the top of the window stack. If 'alwaysAtFront' is true
+# then keep the window at the front.
+#
 def front(screen, alwaysAtFront=False):
-    screen._root.attributes("-topmost", True)
-    if not alwaysAtFront:
-        screen._root.attributes("-topmost", False)
+	screen._root.attributes("-topmost", True)
+	if not alwaysAtFront:
+		screen._root.attributes("-topmost", False)
 
+#
+# Setting 'full' to true will make the window fullscreen.
+#
 def fullscreen(screen, full=True):
-    screen._root.attributes("-fullscreen", full)
+	screen._root.attributes("-fullscreen", full)
 
+#
+# Set the opacity (0 -> 1) of the window.
+#
 def opacity(screen, alpha=0.5):
-    screen._root.attributes("-alpha", alpha)
+	screen._root.attributes("-alpha", alpha)
 
+#
+# Set 'disabled' to true to disable window management functions including
+# maximise, minimise, move & close.
+#
 def disabled(screen, disabled=True):
-    screen._root.attributes("-disabled", disabled)
+	screen._root.attributes("-disabled", disabled)
 
+#
+# Setting 'tool' to true changes the title bar decoration to a toolbar.
+#
 def toolwindow(screen, tool=True):
-    screen._root.attributes("-toolwindow", tool)
+	screen._root.attributes("-toolwindow", tool)
 
+#
+# Setting 'x' and 'y' to true prevents window resizing in the horizontal
+# and vertical direction respectively.
+#
 def noresize(screen, x=False, y=False):
-    screen._root.resizable(x, y)
+	screen._root.resizable(x, y)
 
+#
+# Setting 'nodecorate' to true removes the windows decoration including title bar.
+#
 def nodecorate(screen, nodecorate=True):
-    screen._root.overrideredirect(nodecorate)
+	screen._root.overrideredirect(nodecorate)
 
+#
+# Set the minimum size of the window in pixels.
+#
 def minsize(screen, width=None, height=None):
-    screen._root.minsize(width, height)
+	screen._root.minsize(width, height)
 
+#
+# Set the maximum size of the window in pixels.
+#
 def maxsize(screen, width=None, height=None):
-    screen._root.maxsize(width, height)
+	screen._root.maxsize(width, height)
 
+#
+# Minimise the window.
+#
 def minimise(screen):
-    screen._root.iconify()
+	screen._root.iconify()
 def minimize(screen):
-    minimise(screen)
+	minimise(screen)
 
-def maximise(screen):
-    screen._root.deiconify()
-def maximize(screen):
-    maximise(screen)
+#
+# Restore the window.
+#
+def restore(screen):
+	screen._root.deiconify()
